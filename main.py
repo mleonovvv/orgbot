fd261bfaa9eeb21c9cb1c24a25908739fce674b7
Mikhail Leonov <lm@kodix.ru>
2021-05-16 15:41:00 +0300

    [n]

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # bot.py
 import os
-import sqlite3
 import re
 import discord
-import random
 from datetime import datetime, timedelta, time
 from dotenv import load_dotenv
 from discord.ext import tasks, commands
@@ -20,60 +18,32 @@ client = discord.Client()
 discord.Intents.reactions = True
 discord.Intents.members = True
 
-MESSAGE_DICT = {
-    "message_id": "",
-    "game_name": "",
-    "date": ""
-    }
+@tasks.loop(seconds=50.0)
+async def slow_count():
+    res = db.all()
+    cur_date = datetime.now().strftime("%Y-%m-%d %H:%M")
+    for r in res:
+        if r["date"] == cur_date:
+            msg = ''
+            print(f'cur_date: {cur_date}')
+            r = db.get(where("date") == cur_date)
+            print(f'Alert for id: {r["author_id"]}')
+            channel = client.get_channel(r["channel_id"])
+            if r["author_id"]:
+                for i in r["author_id"]:
+                    msg = msg + f'<@{i}> '
 
-t = "2021-04-25 17:01:10"
-
-# @tasks.loop(seconds=1.0)
-# async def slow_count():
-#     #print(slow_count.current_loop)
-
-#     conn = sqlite3.connect('./bot.db')
-    
-#     cursor = conn.execute('select users,datetime from main')
-    
-#     for i in cursor:
-#         print(i)
-#         #t = i[1]
-#         t = "2021-04-25 17:22:15"
-#         users = i[0]
-#         print(t)
-#         if t:
-#             print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    
-#             if datetime.now().strftime("%Y-%m-%d %H:%M:%S") == t:
-#                 print(f"Пора! {users}")
-#                 channel = client.get_channel(835802932456325240)
-#                 await channel.send('<@693168468702265477>  пора!')
-
-#     conn.close()
-
-
-
+                await channel.send(f'{msg} {r["game_name"]}!')
+            else:
+                msg = f'Сейчас могли бы играть в {r["game_name"]}'
+                await channel.send(f'{msg} {r["game_name"]}!')
+        else:
+            print("Nothing to alert")
 
 @client.event
 async def on_ready():
-    # conn = sqlite3.connect('./bot.db')
-
-    # # Create table
-    # conn.execute('''create table if not exists main
-    #     (
-    #     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    #     name text,
-    #     users text,
-    #     datetime datetime
-    #     )'''
-    # )
-    # conn.commit
-    # conn.close
-
-    #slow_count.start()
+    slow_count.start()
     print(f'{client.user} has connected to Discord!')
-
 
 async def responce(message, game_name, author_name_list, date):
     names = ''''''
@@ -84,15 +54,21 @@ async def responce(message, game_name, author_name_list, date):
         names = names + f'''{c} - {author_name}\n'''
         c += 1
 
-    response = f'сегодня {game_name} в {date.strftime("%H:%M")}. Подписались: \n{names}'
-    res = await message.channel.send(response)
-    print(res.id)
+    if author_name_list:
+        response = f'сегодня {game_name} в {date.strftime("%H:%M")}. Подписались: \n{names}'
+        res = await message.channel.send(response)
+    else:
+        response = f'сегодня хотели {game_name} в {date.strftime("%H:%M")}, но все отписались'
+        res = await message.channel.send(response)
 
-    MESSAGE_DICT.update({
-                        "message_id": res.id,
-                        "game_name": game_name,
-                        "date": str(date)
-                        })
+    print(f'game_name: {game_name}, date: {date}')
+
+    r = db.update(
+        {"message_id": res.id},
+        where('game_name') == game_name and where('date') == date.strftime("%Y-%m-%d %H:%M")
+    )
+
+    print(f'update: {r}')
 
     emojis = ['➕', '➖']
 
@@ -104,61 +80,48 @@ async def subscribe(reaction, game_name, game_date, user):
     print(f"game_name: {game_name}")
     print(f"game_date: {game_date}")
 
-    res = db.search(where('game_name') == game_name and where('date') == game_date)
-    print(f'res: {res}')
-    if user.name in res[0]["author_name"]:
+    res = db.get(where('message_id') == reaction.message.id)
+    
+    if user.name in res["author_name"]:
         response = f'Пользователь {user.name} уже подписан'
         res = await reaction.message.channel.send(response)
     else:
-        add_author_name_list = res[0]["author_name"]
+        add_author_name_list = res["author_name"]
         add_author_name_list.append(user.name)
-
-        add_author_id_list = res[0]["author_id"]
+        add_author_id_list = res["author_id"]
         add_author_id_list.append(user.id)
-
-        db.update({"author_name": add_author_name_list}, where('game_name') == game_name and where('date') == game_date)
-        db.update({"author_id": add_author_id_list}, where('game_name') == game_name and where('date') == game_date)
-
-        date_in_db = datetime.strptime(game_date, "%Y-%m-%d %H:%M:%S")
+        db.update({"author_name": add_author_name_list}, where('message_id') == reaction.message.id)
+        db.update({"author_id": add_author_id_list}, where('message_id') == reaction.message.id)
+        date_in_db = datetime.strptime(game_date, "%Y-%m-%d %H:%M")
         cur_date = datetime.now()
+
         if date_in_db.date() == cur_date.date():
             response = f'Пользователь {user.name} подписан на {game_name} сегодня в {date_in_db.strftime("%H:%M")}'
         else:
-            response = f'Пользователь {user.name} подписан на {game_name} в {str(date_in_db)}'
+            response = f'Пользователь {user.name} подписан на {game_name} в {date_in_db.strftime("%d %b %H:%M")}'
+
         res = await reaction.message.channel.send(response)
 
 async def unsubscribe(reaction, game_name, game_date, user):
+    res = db.get(where('message_id') == reaction.message.id)
 
-    print(f"game_name: {game_name}")
-    print(f"game_date: {game_date}")
-
-    res = db.search(where('game_name') == game_name and where('date') == game_date)
-    print(f'res: {res}')
-    print(f'author_name-unsubscribe: {res[0]["author_name"]}')
-
-    if user.name in res[0]["author_name"]:
-        add_author_name_list = res[0]["author_name"]
+    if user.name in res["author_name"]:
+        add_author_name_list = res["author_name"]
         add_author_name_list.remove(user.name)
-
-        add_author_id_list = res[0]["author_id"]
+        add_author_id_list = res["author_id"]
         add_author_id_list.remove(user.id)
-
-        db.update({"author_name": add_author_name_list}, where('game_name') == game_name and where('date') == game_date)
-        db.update({"author_id": add_author_id_list}, where('game_name') == game_name and where('date') == game_date)
-
-        date_in_db = datetime.strptime(game_date, "%Y-%m-%d %H:%M:%S")
-
+        db.update({"author_name": add_author_name_list}, where('message_id') == reaction.message.id)
+        db.update({"author_id": add_author_id_list}, where('message_id') == reaction.message.id)
+        date_in_db = datetime.strptime(game_date, "%Y-%m-%d %H:%M")
         response = f'Пользователь {user.name} отписан от {game_name} в {str(date_in_db)}'
         res = await reaction.message.channel.send(response)        
-
     else:
-        response = f'Пользователь {user.name} не подписан на {game_name}'
+        response = f'Пользователь {user.name} не подписан на {game_name}' 
         res = await reaction.message.channel.send(response)
         
 @client.event
 async def on_message(message):
-    global MESSAGE_DICT
-    global MESSAGE_ID
+
     if message.author == client.user:
         return
 
@@ -170,32 +133,31 @@ async def on_message(message):
     # ADD
 
     if match_result_add:
-        
         print(message)
         game_name = match_result_add.groups()[1]
         time = match_result_add.groups()[2]
         print(f'message.author - {message.author}')
         author_id_list = []
         author_name_list = []
-
         author_id_list.append(message.author.id)
         author_name_list.append(message.author.name)
-
         t = datetime.today().strftime('%Y-%m-%d') + "/" + time
-
         date = datetime.strptime(t, '%Y-%m-%d/%H:%M')
+        print(f'message.channel.id: {message.channel.id}')
 
         # Insert a row of data
         db.insert({
+            'message_id': "",
+            'channel_id': message.channel.id,
             'game_name': game_name, 
             'author_id': author_id_list,
             'author_name': author_name_list,
-            'date': str(date)
+            'date': date.strftime("%Y-%m-%d %H:%M")
             })
 
-        time = str(date.time())
         await responce(message, game_name, author_name_list, date)
  
+    # STATUS
 
     if message.content == '!четам':
 
@@ -207,55 +169,17 @@ async def on_message(message):
                 game_name = r["game_name"]
                 author_name = r["author_name"]
                 date = r["date"]
-
-                date_in_db = datetime.strptime(r["date"], "%Y-%m-%d %H:%M:%S")
+                date_in_db = datetime.strptime(r["date"], "%Y-%m-%d %H:%M")
                 cur_date = datetime.now()
                 
                 if date_in_db.date() == cur_date.date():
-                    
-                    # MESSAGE_DICT.update({
-                    #     "message_id": res.id,
-                    #     "game_name": r["game_name"],
-                    #     "date": r["date"]
-                    #     })
-
                     await responce(message, game_name, author_name, date_in_db)
-                    # time = str(date_in_db.time())
-                    # response = f'сегодня {r["game_name"]} в {time}. Подписались: {r["author_name"]}'
-                    # res = await message.channel.send(response)
-                    # print(res.id)
-                    
-
-                    # emojis = ['➕', '➖']
-
-                    # for emoji in emojis:
-                    #     await res.add_reaction(emoji)
-
                 else:
                     print("Else")
                     response = f'На сегодня игорей нет'
                     res = await message.channel.send(response)
                     print(f'сообщенее отправлено - {res.id}')
 
-
-        #         row_id = row[0]
-        #         game_name = row[1]
-        #         user_id = row[2]
-
-        #         print(f'user_id = {user_id}')
-        #         user = client.get_user(693168468702265477)
-        #         print(f'user - {user}')
-
-                
-        #         res = await message.channel.send(response)
-
-                # emojis = ['➕', '➖']
-
-                # for emoji in emojis:
-                #     await res.add_reaction(emoji)
-
-        #         print(f'сообщенее отправлено - {res.id}')
-        #         MESSAGE_DICT.update({res.id: {"game_id": row[0]}})
         else:
             print("Else")
             response = f'База расписаний пустая'
@@ -266,6 +190,8 @@ async def on_message(message):
 
     pattern_rm = re.compile("^(!rm)\s(.*)")
     match_result_rm = re.match(pattern_rm, message.content)
+
+    # RM 
 
     # if match_result_rm:
 
@@ -292,45 +218,39 @@ async def on_message(message):
 #    if user.bot:
 #        return
 
-
-
-# @client.event
-# async def on_raw_reaction_add(payload): 
-#     print(payload.emoji.name)
-#     if payload.emoji.name == "➕" and payload.message_id == message_id:
-#         print(f"{payload.member.name} - подписался")
-#         #await client.add_roles(user, Role)
-
-
-
-
 @client.event
 async def on_reaction_add(reaction, user):
     if user.bot:
         return
 
     print(f'reaction.message.id = {reaction.message.id}')
-    #print(f'MESSAGE_ID = {MESSAGE_ID}')
+     #print(f'MESSAGE_ID = {MESSAGE_ID}')
+    res = db.all()
+    messages_list = []
+    for i in res:
+        messages_list.append(i["message_id"])
 
-    if reaction.emoji == "➕" and reaction.message.id == MESSAGE_DICT['message_id']:
-        print(f'USER {user}')
-        print(f'ID {user.id}')
-        game_name = MESSAGE_DICT["game_name"]
-        game_date = MESSAGE_DICT["date"]
-        #response = f'{user} - подписался'
-        await subscribe(reaction, game_name, game_date, user)
-        #await reaction.message.channel.send(response)
-    else:
-        print(f"reaction.emoji not equal ➕: {reaction.emoji} or reaction.message.id not equal {reaction.message.id}: {MESSAGE_DICT['message_id']}")
+    if reaction.message.id in messages_list:
 
-    if reaction.emoji == "➖" and reaction.message.id == MESSAGE_DICT['message_id']:
-        game_name = MESSAGE_DICT["game_name"]
-        game_date = MESSAGE_DICT["date"]
-        await unsubscribe(reaction, game_name, game_date, user)
-        print(f"{user} - отписался")
-        #response = f'{user} - отписался'
-        #await reaction.message.channel.send(response)
-    else:
-        print("Else minus")
+        if reaction.emoji == "➕":
+            res = db.get(where('message_id') == reaction.message.id)
+            game_name = res["game_name"]
+            game_date = res["date"]
+            #response = f'{user} - подписался'
+            await subscribe(reaction, game_name, game_date, user)
+            #await reaction.message.channel.send(response)
+        else:
+            print(f"reaction.emoji not equal ➕: {reaction.emoji}")
+
+        if reaction.emoji == "➖":
+            res = db.get(where('message_id') == reaction.message.id)
+            game_name = res["game_name"]
+            game_date = res["date"]
+            await unsubscribe(reaction, game_name, game_date, user)
+            print(f"{user} - отписался")
+            #response = f'{user} - отписался'
+            #await reaction.message.channel.send(response)
+        else:
+            print("Else minus")
 
 client.run(TOKEN)
